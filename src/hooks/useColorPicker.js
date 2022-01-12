5217c8c82eab321f52c08e619b5d323ab34ba726
DanielRGilles <danryangilles@gmail.com>
2022-01-12 11:40:14 -0800

    initial commit and writing of useColorPicker

--- src/hooks/useColorPicker.js
+++ src/hooks/useColorPicker.js
@@ -1,3 +1,30 @@
-export default function useColorPicker() {
+import { useState } from "react"
+
+export default function useColorPicker(initialState) {
+  const [ formState, setFormState ] = useState(initialState)
   // TODO: Implement me!
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState((prevState) => {
+      return { ...prevState, [name]: {value}}
+    })
+    switch (name) {
+      case 'fgColor':
+        setFgColor(value);
+        setDidChangeColor(true);
+        break;
+      case 'bgColor':
+        setBgColor(value);
+        setDidChangeColor(true);
+        break;
+      case 'content':
+        setDidChangeColor(false);
+        setContent(value);
+        break;
+      default:
+        break;
+    }
+    return [formState, handleChange]
+    
+  };
 }
